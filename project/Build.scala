1ecee2781244f3ddda293fbd93f1f988e99f94ad
jto <jtournay@linkedin.com>
2012-07-25 03:08:56 -0700

    artifactory conf

--- project/Build.scala
+++ project/Build.scala
@@ -3,9 +3,12 @@ import sbt.Defaults._
 import sbt.Keys._
 
 object NGPluginBuild extends Build {
-  
-  // TODO: hard coded path...
-  val localrepo = Resolver.file("Play local repo",  new File(Path.userHome.absolutePath + "/Documents/jto_Play20/repository/local"))(Resolver.ivyStylePatterns)
+
+  val artifactory = "http://artifactory.corp.linkedin.com:8081/artifactory/"
+  val releases = "Artifactory releases"  at artifactory + "release"
+  val snapshots = "Artifactory snapshots"  at artifactory + "snapshot"
+  val sandbox = "Artifactory sandbox" at artifactory + "ext-sandbox"
+  val local = Resolver.file("Play local repo",  new File(Path.userHome.absolutePath + "/Documents/jto_Play20/repository/local"))(Resolver.ivyStylePatterns)
 
   lazy val root = Project("root", file("."),
     settings = commonSettings ++ Seq(
@@ -16,8 +19,6 @@ object NGPluginBuild extends Build {
     id = "play-testng-helpers",
     base = file("helpers"),
     settings = commonSettings ++ Seq(
-      version := "1.0-SNAPSHOT",
-      crossScalaVersions := Seq("2.9.1"),
       libraryDependencies ++= Seq(
         "org.testng" % "testng" % "6.4" /*% "provided"*/,
         "play" %% "play-test" % "2.1-SNAPSHOT" //% "provided"
@@ -28,8 +29,6 @@ object NGPluginBuild extends Build {
     base = file("plugin"),
     settings = commonSettings ++ Seq(
       sbtPlugin := true,
-      version := "1.0-SNAPSHOT",
-      crossScalaVersions := Seq("2.9.1"),
       libraryDependencies <++= (scalaVersion, sbtVersion) { 
         case (scalaVersion, sbtVersion) => Seq(
           sbtPluginExtra("de.johoop" % "sbt-testng-plugin" % "2.0.2", sbtVersion, scalaVersion),
@@ -40,9 +39,18 @@ object NGPluginBuild extends Build {
   lazy val commonSettings: Seq[Setting[_]] = Project.defaultSettings ++ publishSettings ++ Seq(
     organization := "com.linkedin",
     scalaVersion := "2.9.1",
-    resolvers += localrepo)
+    crossScalaVersions := Seq("2.9.1"),
+    version := "0.0.1",
+    resolvers ++= Seq(snapshots, releases, local))
 
-  lazy val publishSettings: Seq[Setting[_]] = Seq(publishTo := Some(
-    localrepo),
+  lazy val publishSettings: Seq[Setting[_]] = Seq(
+    publishTo <<= version { (v: String) =>
+      if (v.trim.endsWith("SNAPSHOT")) 
+        Some(snapshots) 
+      else
+        Some(releases)
+    },
+    //publishTo := Some(sandbox),
+    credentials += Credentials("Artifactory Realm", "artifactory.corp.linkedin.com", "<login>", "<password>"),
     publishMavenStyle := false)
 }
