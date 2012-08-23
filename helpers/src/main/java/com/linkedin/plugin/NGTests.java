a5e15412dc20070cefc3794bbc095ce3d7a335d2
jto <jtournay@linkedin.com>
2012-08-23 03:16:13 -0700

    Refactoring

--- helpers/src/main/java/com/linkedin/plugin/NGTests.java
+++ helpers/src/main/java/com/linkedin/plugin/NGTests.java
@@ -27,6 +27,106 @@ import play.libs.F.*;
 // TODO: refactor
 public class NGTests implements IHookable {
   
+  private class AnnotationsReader {
+    
+    private ITestResult itr;
+    
+    public AnnotationsReader(ITestResult testResult){
+      itr = testResult;
+    }
+    
+    private Method testMethod(){
+      return itr.getMethod().getConstructorOrMethod().getMethod();
+    }
+
+    private Class testClass(){
+      return itr.getTestClass().getRealClass();
+    }
+
+    private <T extends Annotation> T getAnnotationFromMethodOrClass(Class<T> c){
+      Class clazz = testClass();
+      Method m = testMethod();
+
+      T classAnn = (T)clazz.getAnnotation(c);
+      T a = m.getAnnotation(c);
+
+      if(a != null)
+        return a;
+      else
+        return classAnn;
+    }
+
+    private WithFakeApplication getFakeApp(){
+      return getAnnotationFromMethodOrClass(WithFakeApplication.class);
+    }
+
+    private WithTestServer getTestServer(){
+      return getAnnotationFromMethodOrClass(WithTestServer.class);
+    }
+
+    private Map<String, String> getConf(){
+      Map<String, String> conf = new HashMap<String, String>();
+
+      if(getFakeApp() == null)
+        return conf;
+
+      Class clazz = testClass();
+      Method m = testMethod();
+
+      Confs classConfs = (Confs)clazz.getAnnotation(Confs.class);
+      Conf classConf = (Conf)clazz.getAnnotation(Conf.class);
+      Confs methodConfs = m.getAnnotation(Confs.class);
+      Conf methodConf = m.getAnnotation(Conf.class);
+
+      if(classConfs != null){
+        for(Conf c : classConfs.value())
+          conf.put(c.key(), c.value());
+      }
+
+      if(classConf != null)
+        conf.put(classConf.key(), classConf.value());
+
+      if(methodConfs != null){
+        for(Conf c : methodConfs.value())
+          conf.put(c.key(), c.value());
+      }
+
+      if(methodConf != null)
+        conf.put(methodConf.key(), methodConf.value());
+
+      return conf;
+    }
+
+    private List<String> getPlugins(){
+      Class clazz = testClass();
+      Method m = testMethod();
+
+      WithPlugins classPlugins = (WithPlugins)clazz.getAnnotation(WithPlugins.class);
+      WithPlugins methodPlugins = m.getAnnotation(WithPlugins.class);
+
+      List<String> plugins = new ArrayList<String>();
+      if(classPlugins != null)
+        plugins.addAll(Arrays.asList(classPlugins.value()));
+      if(methodPlugins != null)
+      plugins.addAll(Arrays.asList(methodPlugins.value()));
+
+      return plugins;
+    }
+    
+    private FakeApplication buildFakeApplication(WithFakeApplication fa){
+      if(fa != null){
+        String path = fa.path();
+        return new FakeApplication(new File(path), Helpers.class.getClassLoader(), getConf(), getPlugins());
+      }
+      return null;
+    }
+
+    private TestServer buildTestServer(WithTestServer ts) {
+      FakeApplication fake = buildFakeApplication(ts.fakeApplication());
+      return testServer(ts.port(), fake);
+    }
+  }
+  
   // XXX: Evil hack, may lead to race conditions...
   private TestBrowser _testBrowser = null;
   protected TestBrowser browser(){
@@ -34,107 +134,23 @@ public class NGTests implements IHookable {
       throw new RuntimeException("No TestBrowser available, test class or method must be annotated with @WithTestServer");
     return _testBrowser;
   }
-  
-  private Method testMethod(ITestResult testResult){
-    return testResult.getMethod().getConstructorOrMethod().getMethod();
-  }
-  
-  private Class testClass(ITestResult testResult){
-    return testResult.getTestClass().getRealClass();
-  }
-
-  private <T extends Annotation> T getAnnotationFromMethodOrClass(Class<T> c, ITestResult testResult){
-    Class clazz = testClass(testResult);
-    Method m = testMethod(testResult);
-    
-    T classAnn = (T)clazz.getAnnotation(c);
-    T a = m.getAnnotation(c);
-
-    if(a != null)
-      return a;
-    else
-      return classAnn;
-  }
-
-  private WithFakeApplication getFakeApp(ITestResult testResult){
-    return getAnnotationFromMethodOrClass(WithFakeApplication.class, testResult);
-  }
-
-  private WithTestServer getTestServer(ITestResult testResult){
-    return getAnnotationFromMethodOrClass(WithTestServer.class, testResult);
-  }
-  
-  private Map<String, String> getConf(ITestResult testResult){
-    Map<String, String> conf = new HashMap<String, String>();
-    
-    if(getFakeApp(testResult) == null)
-      return conf;
-    
-    Class clazz = testClass(testResult);
-    Method m = testMethod(testResult);
-    
-    Confs classConfs = (Confs)clazz.getAnnotation(Confs.class);
-    Conf classConf = (Conf)clazz.getAnnotation(Conf.class);
-    Confs methodConfs = m.getAnnotation(Confs.class);
-    Conf methodConf = m.getAnnotation(Conf.class);
-    
-    if(classConfs != null){
-      for(Conf c : classConfs.value())
-        conf.put(c.key(), c.value());
-    }
-    
-    if(classConf != null)
-      conf.put(classConf.key(), classConf.value());
-    
-    if(methodConfs != null){
-      for(Conf c : methodConfs.value())
-        conf.put(c.key(), c.value());
-    }
-    
-    if(methodConf != null)
-      conf.put(methodConf.key(), methodConf.value());
-      
-    return conf;
-  }
-  
-  private List<String> getPlugins(ITestResult testResult){
-    Class clazz = testClass(testResult);
-    Method m = testMethod(testResult);
-
-    WithPlugins classPlugins = (WithPlugins)clazz.getAnnotation(WithPlugins.class);
-    WithPlugins methodPlugins = m.getAnnotation(WithPlugins.class);
-
-    List<String> plugins = new ArrayList<String>();
-    if(classPlugins != null)
-      plugins.addAll(Arrays.asList(classPlugins.value()));
-    if(methodPlugins != null)
-    plugins.addAll(Arrays.asList(methodPlugins.value()));
-
-    return plugins;
-  }
-
-  private FakeApplication buildFakeApplication(WithFakeApplication fa, ITestResult testResult){
-    if(fa != null){
-      String path = fa.path();
-      return new FakeApplication(new File(path), Helpers.class.getClassLoader(), getConf(testResult), getPlugins(testResult));
-    }
-    return null;
-  }
 
   public void run(final IHookCallBack icb, final ITestResult testResult) {
-    WithFakeApplication fa = getFakeApp(testResult);
-    WithTestServer ts = getTestServer(testResult);
+    
+    AnnotationsReader reader = new AnnotationsReader(testResult);
+    
+    WithFakeApplication fa = reader.getFakeApp();
+    WithTestServer ts = reader.getTestServer();
 
     if(fa != null){
-      FakeApplication app = buildFakeApplication(fa, testResult);
+      FakeApplication app = reader.buildFakeApplication(fa);
       start(app);
       icb.runTestMethod(testResult);
       stop(app);
     }
     else if(ts != null){
-      FakeApplication fake = buildFakeApplication(ts.fakeApplication(), testResult);
-      TestServer server =testServer(ts.port(), fake);
-
+      TestServer server = reader.buildTestServer(ts);
+      // TODO: parameterize WebDriver
       running(server, HTMLUNIT, new Callback<TestBrowser>() {
         public void invoke(final TestBrowser browser) {
           _testBrowser = browser;
