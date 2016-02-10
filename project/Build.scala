5211b8584a0667bdf90eadfc1eda7616f3ab3e87
Ruobing Li <rli@linkedin.com>
2016-02-09 19:45:15 -0800

    Bump version number to 2.4.2

--- project/Build.scala
+++ project/Build.scala
@@ -40,7 +40,7 @@ object NGPluginBuild extends Build {
   lazy val commonSettings: Seq[Setting[_]] = Project.defaultSettings ++ Seq(
     organization := "com.linkedin.play-testng-plugin",
     scalaVersion := "2.10.4",
-    version := "2.4.1",
+    version := "2.4.2",
     resolvers ++= Seq(Repos.typeSafeReleases, Repos.scalazReleases)
   )
 }
